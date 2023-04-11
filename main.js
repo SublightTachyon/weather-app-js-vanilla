4a41796802ff61d89140b8d181566fdd319f75fc
SublightTachyon <turone2021@gmail.com>
2023-04-11 03:07:47 -0500

    none

--- main.js
+++ main.js
@@ -3,8 +3,20 @@
     let lat;
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
-            console.log(position)
-        })
+            long = position.coords.longitude;
+            lat = position.coords.latitude;
+            apiKey = 'f54b695fd54a245c73014859276c3be8';
+            const api = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&exclude={part}&appid=${apiKey}`;
+            fetch(api)
+            .then(response =>{
+                return response.json
+            })
+            .then(data =>{
+                console.log(data);
+            })
+        });
+        
+
     }else{
         h1.textContent = "please enable location services"
     }
